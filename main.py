4fe406a017a80df1ebfb16e1d22ad6522eee9a28
Helder De Oliveira <hemarque@gmail.com>
2022-10-08 15:15:30 +0200

    types

--- main.py
+++ main.py
@@ -15,5 +15,15 @@ comments
 
 print("hi there!")
 
-an_operation = 5+2
+an_operation = 5 + 2
 print("the result is", an_operation)
+
+print(type("hi!"))  # str
+print(type(3))  # int
+print(type(5.2))  # float
+print(type(3 == 2))  # bool
+print(type(1j))  # complex
+print(type((1, 2, 3)))  # tuple
+print(type([1, 2, 3]))  # list
+print(type({1, 2, 3}))  # set
+print(type({1: 1, 2: 5, 3: -1}))  # dict
