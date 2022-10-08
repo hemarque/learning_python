b20232587389b5786469ae4a86fa2337295845a8
Helder De Oliveira <hemarque@gmail.com>
2022-10-08 15:51:06 +0200

    playing with text order

--- main.py
+++ main.py
@@ -46,12 +46,16 @@ print(10 / 3)
 print(10 // 3)
 print(10 % 3)
 
-print(3 > 4)
 print(3 < 4)
+print(3 > 4)
 print(3 >= 4)
 print(3 <= 4)
 print(3 >= 3)
 print(3 <= 3)
-print(3 == 4)
+print(3 == 3)
 print(3 != 4)
+
+print("he" < "hello")
+print("he" < "hi")
+print("he" < "ha")
 
