61943faec4cf420965b0681ba3e44636aee8a97c
Helder De Oliveira <hemarque@gmail.com>
2022-10-08 15:46:20 +0200

    playing with boolean operations

--- main.py
+++ main.py
@@ -45,3 +45,13 @@ print("hi! " * 3)
 print(10 / 3)
 print(10 // 3)
 print(10 % 3)
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 >= 3)
+print(3 <= 3)
+print(3 == 4)
+print(3 != 4)
+
