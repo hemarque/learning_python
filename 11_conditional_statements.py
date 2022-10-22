3d1ba2c1d6d650ccd435d089a1ff37522ef2ef47
Helder De Oliveira <hemarque@gmail.com>
2022-10-22 22:51:59 +0200

    playing with conditionals

---
+++ 11_conditional_statements.py
@@ -0,0 +1,21 @@
+color = ""
+if color == "red":
+    print("R")
+elif color == "green":
+    print("G")
+elif color == "blue":
+    print("B")
+else:
+    print("not RGB")
+
+word = ""
+if not word:
+    print("word is empty")
+elif word:
+    print("word is not empty")
+
+word2 = "abc"
+if not word2:
+    print("word is empty")
+elif word2:
+    print("word is not empty")
