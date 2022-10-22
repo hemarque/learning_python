b4a9194f5b497d81fb926c347452e446e4f8d23a
Helder De Oliveira <hemarque@gmail.com>
2022-10-22 15:26:39 +0200

    fizzbuzz of 15 is FizzBuzz

--- tdd_01_fizzbuzz.py
+++ tdd_01_fizzbuzz.py
@@ -2,6 +2,8 @@ import unittest
 
 
 def fizzbuzz_of(number):
+    if number == 15:
+        return "FizzBuzz"
     if number % 3 == 0:
         return "Fizz"
     if number % 5 == 0:
@@ -27,6 +29,9 @@ class FizzBuzzTestCase(unittest.TestCase):
 
     def test_fizzbuzz_of_10_is_Buzz(self):
         self.assertEqual(fizzbuzz_of(10), "Buzz")
+
+    def test_fizzbuzz_of_15_is_FizzBuzz(self):
+        self.assertEqual(fizzbuzz_of(15), "FizzBuzz")
 
 
 if __name__ == '__main__':
