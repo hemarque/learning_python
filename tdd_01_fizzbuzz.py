1c1fa690dfecec2cb4f20cb40f7d9f1fb88a562b
Helder De Oliveira <hemarque@gmail.com>
2022-10-22 15:08:32 +0200

    fizzbuzz of 1 is 1

--- tdd_01_fizzbuzz.py
+++ tdd_01_fizzbuzz.py
@@ -1,9 +1,13 @@
 import unittest
 
 
+def fizzbuzz_of(number):
+    return 1
+
+
 class FizzBuzzTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_fizzbuzz_of_1_is_1(self):
+        self.assertEqual(fizzbuzz_of(1), 1)
 
 
 if __name__ == '__main__':
