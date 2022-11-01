c8fac16ab6d56e612f3e698833e9075b661aaedd
Helder De Oliveira <hemarque@gmail.com>
2022-11-01 14:59:44 +0100

    Romans of 4 is IV

--- RomanNumerals.py
+++ RomanNumerals.py
@@ -3,6 +3,9 @@ import unittest
 
 def toRoman(param):
     response = ""
+    if param == 4:
+        response += "IV"
+        param -= 4
     while param >= 1:
         response += "I"
         param -= 1
@@ -14,6 +17,7 @@ class RomanNumeralTranslator(unittest.TestCase):
         self.assertEqual("I", toRoman(1))
         self.assertEqual("II", toRoman(2))
         self.assertEqual("III", toRoman(3))
+        self.assertEqual("IV", toRoman(4))
 
 
 if __name__ == '__main__':
