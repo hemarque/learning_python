9c83d5f9531a2944c34c9e6a57e364d42e6e5955
Helder De Oliveira <hemarque@gmail.com>
2022-11-01 14:55:57 +0100

    Romans of 2 is II

--- RomanNumerals.py
+++ RomanNumerals.py
@@ -2,12 +2,15 @@ import unittest
 
 
 def toRoman(param):
+    if(param == 2):
+        return "II"
     return "I"
 
 
 class RomanNumeralTranslator(unittest.TestCase):
     def testRomans(self):
         self.assertEqual("I", toRoman(1))
+        self.assertEqual("II", toRoman(2))
 
 
 if __name__ == '__main__':
